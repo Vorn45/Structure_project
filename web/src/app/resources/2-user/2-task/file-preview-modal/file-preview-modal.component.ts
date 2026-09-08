70ea6b0f5c4fc9e9e6118ae0b2d4a3c267d4633a
Vorn45 <pisethpanhavorn544@gmail.com>
2026-09-08 14:10:14 +0700

    fix(preview): import CommonModule in file-preview-modal.component.ts

--- web/src/app/resources/2-user/2-task/file-preview-modal/file-preview-modal.component.ts
+++ web/src/app/resources/2-user/2-task/file-preview-modal/file-preview-modal.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component, HostListener, input, output, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
