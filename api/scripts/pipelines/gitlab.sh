ffc87393a582325a1f9b1c9fe937218e555c1997
Brusmuny Pum <brusmunypum.com>
2026-09-09 19:50:25 +0700

    chore: clean useless files, add .dockerignore, and fix npm network resilience in Dockerfiles

--- api/scripts/pipelines/gitlab.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# create tmp vault password file
-cat <<EOF > /tmp/vault-passphrase
-${VAULT_PASS}
-EOF
-
-ansible-playbook ansible/deployment.yml --vault-password-file /tmp/vault-passphrase
-
-rm /tmp/vault-passphrase
